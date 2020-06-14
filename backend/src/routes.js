8116a24c2358b16cadec0c0c4e29dedc3f80c609
jpc0rrea <joaopedrocorrea14@gmail.com>
2020-06-14 11:58:16 -0300

    Teste

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,18 +1,10 @@
 const express = require("express");
-<<<<<<< HEAD
 const SessionController = require("./controllers/SessionController");
-=======
-const SessionController = require('./controllers/SessionController')
->>>>>>> d0d6bf2d0993186d06507d2f8b9355e4ddb85a91
 
 const routes = express.Router();
 
 routes.post("/sessions", SessionController.store);
 
-<<<<<<< HEAD
 module.exports = routes;
-=======
-module.exports = routes
 
 // oba kaduzada
->>>>>>> d0d6bf2d0993186d06507d2f8b9355e4ddb85a91
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-<<<<<<< HEAD
 const routes = require("./routes");
 
 const app = express();
 
 mongoose.connect(
-  "mongodb+srv://omnistack1:omnistack1@aircnc-th3ni.mongodb.net/aircnc?retryWrites=true&w=majority",
+  "mongodb+srv://omnistack:omnistack@aircnc-9tcgr.mongodb.net/aircnc?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    userUnifiedTopology: true,
   }
 );
-=======
-const routes = require('./routes');
-
-const app = express();
-
-mongoose.connect('mongodb+srv://omnistack:omnistack@aircnc-9tcgr.mongodb.net/aircnc?retryWrites=true&w=majority',{
-    useNewUrlParser: true,
-    userUnifiedTopology: true,
-})
-
->>>>>>> d0d6bf2d0993186d06507d2f8b9355e4ddb85a91
 
 app.use(express.json());
 app.use(routes);
 
 app.listen(3333);
-
-
